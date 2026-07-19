9d5a2155cc19934fa81bda59f779095466266db9
lovechauhan25112003 <166515855+lovechauhan25112003@users.noreply.github.com>
2026-07-20 00:24:37 +0530

    Add files via upload

---
+++ _3type()_&_tpyeCasting.py
@@ -0,0 +1,51 @@
+'''type()- used to find the type of data type '''
+# a=31
+# print(type(a))
+
+
+
+'''Type Casting :  -means converting one data type into another
+eg- String->Interger->Float
+
+type-
+1.  Implicit Type Casting(Automatic)-  python automatic converts one data type to another when needed (when possible)
+eg-'''
+
+# x=10
+# y=20.10
+# z=x+y  #----> int+float = float
+# print(z)   #-- >20.10
+# print(type(z))
+
+
+# a=True
+# b=a+5
+# print(b)    #6
+# print(type(b))  # int
+
+
+# a=5
+# b=2+3j
+# c=a+b
+# print(c)          #(7+3j)
+# print(type(c))    #complex
+
+
+# a=2.5
+# b=1+1j
+# c=a+b
+# print(c)        #(3.5+1j)
+# print(type(c))  #complex
+
+# x=True+2.5
+# print(x) 
+# print(type(x))
+
+'''2.  Explicit Type Casting (Mannual)-   mannual convert one type into anothe using functio like 
+int --> float()
+str---> bool()
+eg- '''
+# a='10'
+# b=int(a)   #convert string to integer 
+# print(b+20)++*--
+
