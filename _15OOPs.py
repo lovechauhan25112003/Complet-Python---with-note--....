63c357b639cdcc064e5deca338bd7812566f2e01
lovechauhan25112003 <166515855+lovechauhan25112003@users.noreply.github.com>
2026-08-05 23:17:27 +0530

    Add files via upload

---
+++ _15OOPs.py
@@ -0,0 +1,59 @@
+'''
+
+
+OOPS - Object Oriented Programming S/y / Structure
+
+OOPs stand for object oriented programming language, the main purpose of the oops is to deal with real world entity using programing language.
+
+other way- 
+we can say that oop is a programing paradigm / Methodology
+
+"Paradigm can also be termed as method to solve some problem or do some task. Programing paradigm is an approach to solved problem using some programing
+language. or also we can say it is a method to solved a problem using tools and techniques that using tools and techniques that are available to us following approach.
+
+programming pradigm-
+    - object oriented paradigm 
+    - procedural paradigm 
+    - functional paradigm 
+    - logical paradigm
+    - structured paradigm
+
+
+    
+This concep  is focus  on using reuable code and implement DRY(Dont Repeat Yourself) principle 
+
+6 main pillars or features  of opps are- 
+
+i)-       Class
+ii)-      Object and methods
+iii)-     Inheritance
+iv)-      Polymorphism
+v)-       Abstuction
+vi)-      Encapsulation
+
+Real world example- 1
+
+Animal- eat(), run(),- Dog, Cat
+
+here, Animal - class
+      eat(), run()- methods
+      dog , cat - objects
+
+Real world example- 2
+     Birds                                      Vehical
+     /   \                                       /    \
+sparrow  peacock                              Bike    Car
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
